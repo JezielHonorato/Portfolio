9ae17e258afbc08df8d5d35c866c9a03d710c67f
JezielHonorato <jezielh.ss@gmail.com>
2024-12-31 13:47:10 -0300

    funcao copiar txt

--- PortfolioJezielHonorato/src/app/components/home/home.component.ts
+++ PortfolioJezielHonorato/src/app/components/home/home.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: false,
-  
+
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  copyToClipboard(content: HTMLElement) {
+    const text = content.innerText;
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert(`Texto copiado: ${text}`);
+      })
+      .catch((err) => {
+        console.error('Erro ao copiar o texto: ', err);
+      });
+  }
 }
